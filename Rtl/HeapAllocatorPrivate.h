6eb6b355b8b232dfefdf8ab3de0b6d93ba152678
Trent Nelson <trent@trent.me>
2018-01-21 20:19:51 -0500

    Refactor projects a little, move heap allocator into Rtl.

---
+++ Rtl/HeapAllocatorPrivate.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Memory.h"
+
+MALLOC DefaultHeapMalloc;
+CALLOC DefaultHeapCalloc;
+REALLOC DefaultHeapRealloc;
+FREE DefaultHeapFree;
+
+// vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
